9ef8d12b2a71ea98c430ff2b2517d5a71dc3540c
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-08-19 15:25:50 +0300

    add - constants

---
+++ lib/product/constants/assets_constants.dart
@@ -0,0 +1,3 @@
+class AssetsConstants {
+  static const String logo = 'assets/logo/chillflix_logo.png';
+}
---
+++ lib/product/constants/color_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Color redColor = Color(0xFFdb0000);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color whiteColor = Color(0xFFffffff);
+  static const Color greyColor = Color(0xFF564d4d);
+  static const Color redDarkColor = Color(0xFF831010);
+}
