d52acb573c3340c5e445d025e8ef5dc3f0171398
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-08-22 16:35:49 +0300

    add - constants

--- lib/product/constants/assets_constants.dart
+++ lib/product/constants/assets_constants.dart
@@ -1,4 +1,5 @@
 class AssetsConstants {
   static const String logo = 'assets/logo/chillflix_logo.png';
   static const String banner = 'assets/images/drive_to_survive.jpg';
+  static const String defaultProfilePic = 'assets/images/default_pp.jpg';
 }
